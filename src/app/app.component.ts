608902b90d93b3fa3bd3c9dc0b4a86ae213b5373
Shariful Islam <shariful.islam.mec@gmail.com>
2022-10-02 20:25:23 +0300

    frontend commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { HttpErrorResponse } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Employee } from './employee';
+import { EmployeeService } from './employee.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent  implements OnInit {
   title = 'employeemanager-frontend';
+
+  public employees!: Employee[];
+
+  constructor(private employeeService: EmployeeService) {}
+
+  ngOnInit() {
+      this.getEmployees();
+  }
+
+  public getEmployees(): void {
+    this.employeeService.getEmployees().subscribe({
+      next: (response: Employee[]) => {
+        console.log(this.employees = response);
+      },
+      error: (error: HttpErrorResponse) => {
+        alert(error.message);
+      }
+  });
+  }
 }
